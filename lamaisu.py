8cceda35a4173c51012405f7d5d81f50f2d36fd7
dickshaydle <dickshaydle@users.noreply.github.com>
2020-02-20 01:44:16 +0100

    added wall character

--- lamaisu.py
+++ lamaisu.py
@@ -8,31 +8,39 @@ class Character():
         self.y = y0
         self.symbol = symbol
 
-class Player():
+    def draw(self, stdscr):
+        stdscr.addch(self.y, self.x, self.symbol)
+        # stdscr.refresh()
+
+class Player(Character):
     def __init__(self, x0, y0, symbol = '🦍'):
-        self.x = x0
-        self.y = y0
-        self.symbol = symbol
+        super().__init__(x0, y0, symbol)
 
 
 player = Player(4, 11, '🦙')
+wall = Character(2, 2, '🧱')
+U = Character(3, 3, 'U')
 
-def update(stdscr):
-    stdscr.addch(player.y, player.x, player.symbol)
-    stdscr.refresh()
+ch_list = [player, wall, U]
+
 
 def main_loop(stdscr):
     # Clear screen
-    stdscr.clear()
+    # stdscr.clear()
     curses.curs_set(0)
 
     while True:
         stdscr.clear()
-        update(stdscr)
+
+        y, x = stdscr.getmaxyx()
+        stdscr.addstr(y-1, 0, 'screen size %dx%d'%(x, y))
+        for ch in ch_list:
+            ch.draw(stdscr)
+
         c = stdscr.getch()
-        if c == curses.KEY_RESIZE:
-            y, x = stdscr.getmaxyx()
-            stdscr.addstr(y-1, 0, 'screen size %dx%d'%(x, y))
+        # if c == curses.KEY_RESIZE:
+        #     y, x = stdscr.getmaxyx()
+        #     stdscr.addstr(y-1, 0, 'screen size %dx%d'%(x, y))
             # stdscr.resize()
         if c in (ord('w'), curses.KEY_UP):
             player.y -= 1
@@ -45,6 +53,7 @@ def main_loop(stdscr):
         elif c == ord('q'):
             break  # Exit the while loop
        
+        stdscr.refresh()
        
 if __name__ == '__main__':
     """Entry point."""
